1a2fca6808661dafa7385335da43248b58b866d9
almas_aitken <almasaitken.career@gmail.com>
2022-06-15 12:28:43 +0600

    vrode completed

---
+++ src/App.js
@@ -0,0 +1,227 @@
+import './App.css';
+import {useState} from 'react';
+import {v4 as uuidv4} from 'uuid';
+
+const buttons = [
+  {
+    type: "all",
+    label: "All",
+  },
+  {
+    type: "active",
+    label: "Active",
+  },
+  {
+    type: "done",
+    label: "Done",
+  },
+];
+
+const toDoItems = [
+  {
+    key: uuidv4(),
+    label: "Have fun",
+  },
+  {
+    key: uuidv4(),
+    label: "Spread Empathy",
+  },
+  {
+    key: uuidv4(),
+    label: "Generate Value",
+  },
+];
+
+function App() {
+  const [itemToAdd, setItemToAdd] = useState("");
+  //arrow declaration => expensive computation ex: API calls
+  const [items, setItems] = useState(toDoItems);
+
+  const [filterType, setFilterType] = useState("");
+
+  const [searchRequest, setSearchRequest] = useState("");
+
+  const handleChangeItem = (event) => {
+    setItemToAdd(event.target.value);
+  };
+
+  const handleAddItem = () => {
+    // mutating !WRONG!
+    // const oldItems = items;
+    // oldItems.push({ label: itemToAdd, key: uuidv4() });
+    // setItems(oldItems);
+
+    // not mutating !CORRECT!
+    setItems((prevItems) => [
+      { label: itemToAdd, key: uuidv4() },
+      ...prevItems,
+    ]);
+
+    setItemToAdd("");
+  };
+
+
+  const handleItemDone = ({ key }) => {
+    //first way
+    // const itemIndex = items.findIndex((item) => item.key === key);
+    // const oldItem = items[itemIndex];
+    // const newItem = { ...oldItem, done: !oldItem.done };
+    // const leftSideOfAnArray = items.slice(0, itemIndex);
+    // const rightSideOfAnArray = items.slice(itemIndex + 1, items.length);
+    // setItems([...leftSideOfAnArray, newItem, ...rightSideOfAnArray]);
+
+    //  second way
+    // const changedItem = items.map((item) => {
+    //   if (item.key === key) {
+    //     return { ...item, done: item.done ? false : true };
+    //   } else return item;
+    // });
+
+    //second way updated
+    setItems((prevItems) =>
+      prevItems.map((item) => {
+        if (item.key === key) {
+          return { ...item, done: !item.done };
+        } else return item;
+      })
+    );
+  };
+
+  const handleItemImportant = ({ key }) => {
+    setItems((prevItems) =>
+      prevItems.map((item) => {
+        if (item.key === key) {
+          return { ...item, important: !item.important };
+        } else return item;
+      })
+    );
+  };
+
+  const handleFilterItems = (type) => {
+    setFilterType(type);
+  };
+
+  const amountDone = items.filter((item) => item.done).length;
+
+  const amountLeft = items.length - amountDone;
+
+  const compareLabel = (searchWord, label) => {
+    for (let i = 0; i < searchWord.length; i++) {
+      if (searchWord[i]!==label[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  const handleDeleteItem = ({key}) => {
+    let target = items.findIndex((item) => item.key === key);
+    setItems((prevItems) =>  {
+      if (target === 0) {
+        return [...prevItems.slice(1)];
+      } else if (target === prevItems.length - 1){
+        return [...prevItems.slice(0, target)];
+      } else {
+        return [...prevItems.slice(0, target), ...prevItems.slice(target+1)];
+      }
+    });
+  };
+
+  const filteredItems =
+    !filterType || filterType === "all"
+      ? items.filter((item) => compareLabel(searchRequest, item.label))
+      : filterType === "active"
+      ? items.filter((item) => !item.done && item.label.startsWith(searchRequest))
+      : items.filter((item) => item.done && item.label.startsWith(searchRequest));
+
+
+  const handleSearch = (event) => {
+    setSearchRequest(event.target.value);
+  };
+
+  return (
+    <div className="todo-app">
+      {/* App-header */}
+      <div className="app-header d-flex">
+        <h1>Todo List</h1>
+        <h2>
+          {amountLeft} more to do, {amountDone} done
+        </h2>
+      </div>
+
+      <div className="top-panel d-flex">
+        {/* Search-panel */}
+        <input
+          type="text"
+          className="form-control search-input"
+          placeholder="type to search"
+          onChange={handleSearch}
+        />
+        {/* Item-status-filter */}
+        <div className="btn-group">
+          {buttons.map((item) => (
+            <button
+              onClick={() => handleFilterItems(item.type)}
+              key={item.type}
+              type="button"
+              className={`btn btn-${
+                filterType !== item.type ? "outline-" : ""
+              }info`}
+            >
+              {item.label}
+            </button>
+          ))}
+        </div>
+      </div>
+
+      {/* List-group */}
+      <ul className="list-group todo-list">
+        {filteredItems.length > 0 &&
+          filteredItems.map((item) => (
+            <li key={item.key} className="list-group-item">
+              <span className={`todo-list-item ${item.done ? "done" : "" } ${item.important ? "important" : ""}`}>
+                <span
+                  className={`todo-list-item-label`}
+                  onClick={() => handleItemDone(item)}
+                >
+                  {item.label}
+                </span>
+
+                <button
+                  type="button"
+                  className="btn btn-outline-success btn-sm float-right"
+                  onClick={() => handleItemImportant(item)}
+                >
+                  <i className="fa fa-exclamation" />
+                </button>
+
+                <button
+                  type="button"
+                  className="btn btn-outline-danger btn-sm float-right"
+                  onClick={() => {handleDeleteItem(item)}}
+                >
+                  <i className="fa fa-trash-o" />
+                </button>
+              </span>
+            </li>
+          ))}
+      </ul>
+
+      {/* Add form */}
+      <div className="item-add-form d-flex">
+        <input
+          value={itemToAdd}
+          type="text"
+          className="form-control"
+          placeholder="What needs to be done"
+          onChange={handleChangeItem}
+        />
+        <button className="btn btn-outline-secondary" onClick={handleAddItem}>
+          Add item
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ test.js
@@ -0,0 +1,3 @@
+const arr = [1, 3, 2, 5, 7];
+
+console.log(arr.slice(0, 4));
